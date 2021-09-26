8b240d164105e01f4102724eb3d098cb5f9052b5
Eduardo Casa <eduardo@focusts.com.br>
2021-09-26 10:31:18 -0300

    Correção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'https://b73a-2804-7f7-a287-2492-25f8-d0e-85ad-8938.ngrok.iong build'
+  apiURL: 'http://b73a-2804-7f7-a287-2492-25f8-d0e-85ad-8938.ngrok.io'
 };
